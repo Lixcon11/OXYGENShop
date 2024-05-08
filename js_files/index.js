b512be6bac0bfc68d01600f944ac26de096fdd7a
Lixcon11 <106622009+Lixcon11@users.noreply.github.com>
2024-05-08 02:10:54 +0200

    Images Updated

--- js_files/index.js
+++ js_files/index.js
@@ -1,6 +1,6 @@
 //import { setAttributesByArray as easySet, divWithImages } from "./helper.js";
 import sliderBuilder from "./sliderBuilder.js";
 
-const showCaseImages = ["../jpg/keyboard.jpg", "../jpg/plane.jpg", "../jpg/moon.jpg"];
+const showCaseImages = ["../jpeg/keyboard.jpeg", "../jpeg/plane.jpeg", "../jpeg/moon.jpeg"];
 
 sliderBuilder("slider", "main", "main__questions", showCaseImages);
