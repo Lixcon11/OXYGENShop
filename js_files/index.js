959d9f3c1d3d69ad0e071df483a28ec8954d8d95
Lixcon11 <106622009+Lixcon11@users.noreply.github.com>
2024-05-08 03:12:15 +0200

    Trying new images

--- js_files/index.js
+++ js_files/index.js
@@ -1,6 +1,8 @@
 //import { setAttributesByArray as easySet, divWithImages } from "./helper.js";
 import sliderBuilder from "./sliderBuilder.js";
 
-const showCaseImages = ["../jpg/keyboard.jpg", "../jpg/plane.jpg", "../jpg/moon.jpg"];
+const showCaseImages = ["https://talkingstuff.net/wp-content/uploads/2019/06/blue-gradient-abstract-hd-wallpaper-1920x1200-4430-300x100.jpg",
+"https://mrwallpaper.com/images/thumbnail/synthwave-grid-mountains-1qxfiiabdb5qle2q.webp",
+"https://s.turbifycdn.com/aah/yhst-68986320726817/find-your-joy-purple-blue-wallpaper-border-bp8124bd-57.gif"];
 
 sliderBuilder("slider", "main", "main__questions", showCaseImages);
